44f9b4ead5c55a278c51aece9aea08d79709ad5d
ShaharShi <Shaharshitrit01@gmail.com>
2020-07-15 14:07:01 +0300

    Search operation everything works

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
-function getCountriesFromServer() {
-
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (countries.length === 0) reject("There is a problem with the resource you are looking for with: 'countries.obj.js'")
-            resolve(countries)
-        }, 2000);
-    })
-} 
 
 $(function() {
-
     const cardsContainer = $('#cardsContainer');
+    const searchValue = $('#searchValue');
+
 
     $('#getCountries').on('click', () => {
 
-        getCountriesFromServer().then((countries) => {
-            draw(countries)
+        getCountriesFromServer().then((result) => {
+            draw(result)
     
         }).catch((err) => {
-            console.log(err)
-        })
-    })
+            console.error(err)
+        });
+    });
+    $('#searchOperation').on('click', () => {
+        searchCountriesFromServer(searchValue).then((result) => {
+            draw(result)
+        }).catch((err) => {
+            console.error(err)
+        });
+    });
 
     function draw(countries) {
         cardsContainer.empty();
-
+        console.log('blala')
         const cardItem = countries.map(country => {
             return getCardItem(country);    
         });
@@ -32,7 +31,7 @@ $(function() {
     }
 
     function getCardItem(country) {
-        const cardWrapper = $('<div></div>').attr('class', 'card-wrapper');
+        const cardWrapper = $('<div></div>').attr('class', 'card-wrapper bg-dark');
         const countryName = $(`<h3>${country.name}</h3>`);
         const flagWrap = $('<div></div>').attr('class', 'flag-img');
         const flagImg = $('<img>').attr('src', country.flag);
@@ -46,3 +45,22 @@ $(function() {
 
 });
 
+function getCountriesFromServer() {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (countries.length === 0) reject("There is a problem with the resource you are looking for with: 'countries.obj.js'")
+            resolve(countries)
+        }, 2000);
+    })
+} 
+function searchCountriesFromServer(searchValue) {
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (countries.length === 0) reject("There is a problem with the resource you are looking for with: 'countries.obj.js'")
+            const result = countries.filter(country => country.name.toLowerCase().includes(searchValue.val().toLowerCase()))
+            resolve(result)
+        }, 2000);
+    })
+} 
